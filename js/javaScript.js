5a4f7de7cd51a021867613491f3db5ed6d86c7c0
Kirill11111 <39949224+Kirill11111@users.noreply.github.com>
2018-07-09 12:52:59 +0300

    Add files via upload

--- js/javaScript.js
+++ js/javaScript.js
@@ -112,36 +112,3 @@ function showPannel4() {
      tabPanels4.style.display='block',
      tabPanels1.style.display='none';
 }
-
-
-// Отправка заявки
-$(document).ready(function() {
-	$('#topForm').submit(function() { // проверка на пустоту заполненных полей. Атрибут html5 — required не подходит (не поддерживается Safari)
-		if (document.form.name.value == '' || document.form.phone.value == '' ) {
-			valid = false;
-			return valid;
-		}
-		$.ajax({
-			type: "POST",
-			url: "mail.php",
-			data: $(this).serialize()
-		}).done(function() {
-			$('.js-overlay-thank-you').fadeIn();
-			$(this).find('input').val('');
-			$('#topForm').trigger('reset');
-		});
-		return false;
-	});
-});
-
-// Закрыть попап «спасибо»
-$('.js-close-thank-you').click(function() { // по клику на крестик
-	$('.js-overlay-thank-you').fadeOut();
-});
-
-$(document).mouseup(function (e) { // по клику вне попапа
-	var popup = $('.popup');
-	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
-		$('.js-overlay-thank-you').fadeOut();
-	}
-});
